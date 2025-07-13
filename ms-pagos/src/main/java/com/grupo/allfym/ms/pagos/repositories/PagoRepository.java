8541d4bdd67a33d6d961b70f7a17beb3e8555476
Davs07 <botsymeisterspieler@gmail.com>
2025-07-13 10:45:11 -0500

    Implement Pago microservice with CRUD operations and DTOs

--- ms-pagos/src/main/java/com/grupo/allfym/ms/pagos/repositories/PagoRepository.java
+++ ms-pagos/src/main/java/com/grupo/allfym/ms/pagos/repositories/PagoRepository.java
@@ -2,6 +2,11 @@ package com.grupo.allfym.ms.pagos.repositories;
 
 import com.grupo.allfym.ms.pagos.entity.Pago;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PagoRepository extends CrudRepository<Pago,Long> {
+    List<Pago> findByVentaId(Long ventaId);
 }
